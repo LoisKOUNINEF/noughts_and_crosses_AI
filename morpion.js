ccd020df6bf8ff87b448c144ededc919a919cc3e
LoisKOUNINEF <lkouninef@gmail.com>
2022-04-22 18:38:05 +0200

    add minmax AI

--- morpion.js
+++ morpion.js
@@ -3,9 +3,9 @@ class Morpion {
 	iaPlayer = 'J2';
 	gameOver = false;
 	gridMap = [
-		[null, null, null],
-		[null, null, null],
-		[null, null, null],
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
 	];
 
 	constructor(firstPlayer = 'J1') {
@@ -35,103 +35,161 @@ class Morpion {
 		return document.getElementById(cellId);
 	}
 
-    getBoardWinner = (board) => {
-        const isWinningRow = ([a, b, c]) => (
-            a !== null && a === b && b === c
-        );
+	getBoardWinner = (board) => {
+		const isWinningRow = ([a, b, c]) => (
+			a !== null && a === b && b === c
+			);
 
-        let winner = null;
+		let winner = null;
 
         // Horizontal
         board.forEach((line) => {
-            if (isWinningRow(line)) {
-                winner = line[0];
-            }
+        	if (isWinningRow(line)) {
+        		winner = line[0];
+        	}
         });
 
         // Vertical
         [0, 1, 2].forEach((col) => {
-            if (isWinningRow([board[0][col], board[1][col], board[2][col]])) {
-                winner = board[0][col];
-            }
+        	if (isWinningRow([board[0][col], board[1][col], board[2][col]])) {
+        		winner = board[0][col];
+        	}
         });
 
         if (winner) {
-            return winner;
+        	return winner;
         }
 
         // Diagonal
         const diagonal1 = [board[0][0], board[1][1], board[2][2]];
         const diagonal2 = [board[0][2], board[1][1], board[2][0]];
         if (isWinningRow(diagonal1) || isWinningRow(diagonal2)) {
-            return board[1][1];
+        	return board[1][1];
         }
 
         const isFull = board.every((line) => (
-			line.every((cell) => cell !== null)
-		));
+        	line.every((cell) => cell !== null)
+        	));
         return isFull ? 'tie' : null;
     }
 
-	checkWinner = (lastPlayer) => {
-        const winner = this.getBoardWinner(this.gridMap);
-        if (!winner) {
-            return;
-        }
+    checkWinner = (lastPlayer) => {
+    	const winner = this.getBoardWinner(this.gridMap);
+    	if (!winner) {
+    		return;
+    	}
 
-        this.gameOver = true;
-        switch(winner) {
-            case 'tie':
-			    this.displayEndMessage("Vous êtes à égalité !");
-                break;
-            case this.iaPlayer:
-                this.displayEndMessage("L'IA a gagné !");
-                break;
-            case this.humanPlayer:
-                this.displayEndMessage("Tu as battu l'IA !");
-                break;
-        }
-	}
+    	this.gameOver = true;
+    	switch(winner) {
+    		case 'tie':
+    		this.displayEndMessage("Vous êtes à égalité !");
+    		break;
+    		case this.iaPlayer:
+    		this.displayEndMessage("L'IA a gagné !");
+    		break;
+    		case this.humanPlayer:
+    		this.displayEndMessage("Tu as battu l'IA !");
+    		break;
+    	}
+    }
 
-	displayEndMessage = (message) => {
-		const endMessageElement = document.getElementById('end-message');
-		endMessageElement.textContent = message;
-		endMessageElement.style.display = 'block';
-	}
+    displayEndMessage = (message) => {
+    	const endMessageElement = document.getElementById('end-message');
+    	endMessageElement.textContent = message;
+    	endMessageElement.style.display = 'block';
+    }
 
-	drawHit = (x, y, player) => {
-		if (this.gridMap[y][x] !== null) {
-			return false;
-		}
+    drawHit = (x, y, player) => {
+    	if (this.gridMap[y][x] !== null) {
+    		return false;
+    	}
 
-		this.gridMap[y][x] = player;
-		this.getCell(x, y).classList.add(`filled-${player}`);
-		this.checkWinner(player);
-		return true;
-	}
+    	this.gridMap[y][x] = player;
+    	this.getCell(x, y).classList.add(`filled-${player}`);
+    	this.checkWinner(player);
+    	return true;
+    }
 
-	doPlayHuman = (x, y) => {
-		if (this.gameOver) {
-			return;
-		}
+    doPlayHuman = (x, y) => {
+    	if (this.gameOver) {
+    		return;
+    	}
 
-		if (this.drawHit(x, y, this.humanPlayer)) {
-			this.doPlayIa();
-		}
-	}
+    	if (this.drawHit(x, y, this.humanPlayer)) {
+    		this.doPlayIa();
+    	}
+    }
 
-	doPlayIa = () => {
-		if (this.gameOver) {
-			return;
-		}
+    minMax = (grid, depth, maximize) => {
+    	let result = this.getBoardWinner(grid);
 
-		let hasPlayed = false;
-		this.gridMap.forEach((line, y) => {
-			line.forEach((cell, x) => {
-				if (!cell && !hasPlayed) {
-					hasPlayed = this.drawHit(x, y, this.iaPlayer);
-				}
-			});
-		});
-	}
+    	const scores = {
+    		'J1': -10,
+    		'J2': 10,
+    		'tie': 0,
+    	}
+
+    	if (result !== null) {
+    		return scores[result];
+    	}
+
+    	if (maximize) {
+    		let bestScore = -10;
+    		grid.forEach((line, y) => {
+    			line.forEach((cell, x) => {
+    				if (!cell) {
+    					grid[y][x] = this.iaPlayer;
+    					let score = this.minMax(grid, depth + 1, false);
+    					grid[y][x] = null;
+    					bestScore = Math.max(score, bestScore);
+    				}
+    			});
+    		});
+
+    		return bestScore;
+    	}
+
+    	else {
+    		let bestScore = 10;
+    		grid.forEach((line, y) => {
+    			line.forEach((cell, x) => {
+    				if (!cell) {
+    					grid[y][x] = this.humanPlayer;
+    					let score = this.minMax(grid, depth + 1, true);
+    					grid[y][x] = null;
+    					bestScore = Math.min(score, bestScore);
+    				}
+    			});
+    		});
+
+    		return bestScore;
+    	}
+    }
+
+    doPlayIa = () => {
+    	if (this.gameOver) {
+    		return;
+    	}
+
+    	let bestScore = -10;
+    	let aiMove;
+    	let grid = [...this.gridMap];
+
+    	grid.forEach((line, y) => {
+    		line.forEach((cell, x) => {
+    			if (!cell) {
+    				grid[y][x] = this.iaPlayer;
+    				let score = this.minMax(grid, 0, false);
+    				grid[y][x] = null;
+
+    				if (score > bestScore) {
+    					bestScore = score;
+    					aiMove = { x, y };
+    				}
+    			}
+    		});
+    	});
+
+    	this.drawHit(aiMove.x, aiMove.y, this.iaPlayer);
+    }
 }
